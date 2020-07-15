15aaaf793d3c4d109c6f78de1eaa303be84686ec
OrangeGamza <orangegamza@gmail.com>
2020-07-15 10:22:38 +0900

    initial commit

---
+++ Assets/Scripts/DBManager.cs
@@ -0,0 +1,621 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class Actor
+{
+    private string actor_id;
+    private string actor_name;
+    private string actor_year;
+    public string Actor_id
+    {
+        get
+        {
+            return actor_id;
+        }
+
+        set
+        {
+            actor_id = value;
+        }
+    }
+
+    public string Actor_name
+    {
+        get
+        {
+            return actor_name;
+        }
+
+        set
+        {
+            actor_name = value;
+        }
+    }
+
+    public string Actor_year
+    {
+        get
+        {
+            return actor_year;
+        }
+
+        set
+        {
+            actor_year = value;
+        }
+    }
+}
+
+public class Act
+{
+    private string act_cast;
+    private string act_actor_id;
+    private string act_stage_id;
+
+    public string Act_cast
+    {
+        get
+        {
+            return act_cast;
+        }
+
+        set
+        {
+            act_cast = value;
+        }
+    }
+
+    public string Act_actor_id
+    {
+        get
+
+        {
+            return act_actor_id;
+        }
+
+        set
+        {
+            act_actor_id = value;
+        }
+    }
+
+    public string Act_stage_id
+    {
+        get
+        {
+            return act_stage_id;
+        }
+
+        set
+        {
+            act_stage_id = value;
+        }
+    }
+}
+
+public class Company
+{
+    private string company_name;
+    private string company_year;
+
+    public string Company_name
+    {
+        get
+        {
+            return company_name;
+        }
+
+        set
+        {
+            company_name = value;
+        }
+    }
+
+    public string Company_year
+    {
+        get
+        {
+            return company_year;
+        }
+
+        set
+        {
+            company_year = value;
+        }
+    }
+}
+
+public class Contribute
+{
+    private string contribute_stage_id;
+    private string contribute_company_name;
+
+    public string Contribute_stage_id
+    {
+        get
+        {
+            return contribute_stage_id;
+        }
+
+        set
+        {
+            contribute_stage_id = value;
+        }
+    }
+
+    public string Contribute_company_name
+    {
+        get
+        {
+            return contribute_company_name;
+        }
+
+        set
+        {
+            contribute_company_name = value;
+        }
+    }
+}
+
+public class Creator
+{
+    private string creator_id;
+    private string creator_name;
+
+    public string Creator_id
+    {
+        get
+        {
+            return creator_id;
+        }
+
+        set
+        {
+            creator_id = value;
+        }
+    }
+
+    public string Creator_name
+    {
+        get
+        {
+            return creator_name;
+        }
+
+        set
+        {
+            creator_name = value;
+        }
+    }
+}
+
+public class Make
+{
+    private string make_stage_id;
+    private string make_creator_id;
+
+    public string Make_stage_id
+    {
+        get
+        {
+            return make_stage_id;
+        }
+
+        set
+        {
+            make_stage_id = value;
+        }
+    }
+
+    public string Make_creator_id
+    {
+        get
+        {
+            return make_creator_id;
+        }
+
+        set
+        {
+            make_creator_id = value;
+        }
+    }
+}
+
+public class Play
+{
+    private string play_loc;
+    private string play_stage_id;
+    private string play_theater_name;
+
+    public string Play_loc
+    {
+        get
+        {
+            return play_loc;
+        }
+
+        set
+        {
+            play_loc = value;
+        }
+    }
+
+    public string Play_stage_id
+    {
+        get
+        {
+            return play_stage_id;
+        }
+
+        set
+        {
+            play_stage_id = value;
+        }
+    }
+
+    public string Play_theater_name
+    {
+        get
+        {
+            return play_theater_name;
+        }
+
+        set
+        {
+            play_theater_name = value;
+        }
+    }
+}
+
+public class Stage
+{
+    private string stage_id;
+    private string stage_name;
+    private string stage_year;
+    private string stage_season;
+
+    public string Stage_id
+    {
+        get
+        {
+            return stage_id;
+        }
+
+        set
+        {
+            stage_id = value;
+        }
+    }
+
+    public string Stage_name
+    {
+        get
+        {
+            return stage_name;
+        }
+
+        set
+        {
+            stage_name = value;
+        }
+    }
+
+    public string Stage_year
+    {
+        get
+        {
+            return stage_year;
+        }
+
+        set
+        {
+            stage_year = value;
+        }
+    }
+
+    public string Stage_season
+    {
+        get
+        {
+            return stage_season;
+        }
+
+        set
+        {
+            stage_season = value;
+        }
+    }
+}
+
+public class Theater
+{
+    private string theater_name;
+    private string theater_location;
+
+    public string Theater_name
+    {
+        get
+        {
+            return theater_name;
+        }
+
+        set
+        {
+            theater_name = value;
+        }
+    }
+
+    public string Theater_location
+    {
+        get
+        {
+            return theater_location;
+        }
+
+        set
+        {
+            theater_location = value;
+        }
+    }
+}
+
+public class DB
+{
+    private static DB db = null;
+    protected DB() { }
+    public static DB Data()
+    {
+        if (db == null)
+            db = new DB();
+        return db;
+    }
+
+    public List<Actor> Actors;
+    public List<Act> Acts;
+    public List<Company> Companies;
+    public List<Contribute> Contributes;
+    public List<Creator> Creators;
+    public List<Make> Makes;
+    public List<Play> Plays;
+    public List<Stage> Stages;
+    public List<Theater> Theaters;
+}
+
+public class DBManager : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+        XElement xActor = XElement.Load("Assets/Resources/Database/ACTOR.xml");
+        XElement xAct = XElement.Load("Assets/Resources/Database/ACT.xml");
+        XElement xCompany = XElement.Load("Assets/Resources/Database/COMPANY.xml");
+        XElement xContribute = XElement.Load("Assets/Resources/Database/CONTRIBUTE.xml");
+        XElement xCreator = XElement.Load("Assets/Resources/Database/CREATOR.xml");
+        XElement xMake = XElement.Load("Assets/Resources/Database/MAKE.xml");
+        XElement xPlay = XElement.Load("Assets/Resources/Database/PLAY.xml");
+        XElement xStage = XElement.Load("Assets/Resources/Database/STAGE.xml");
+        XElement xTheator = XElement.Load("Assets/Resources/Database/THEATER.xml");
+        
+        List<Actor> actorList = new List<Actor>();
+        int three = 0;
+        int idx = -1;
+        foreach (var actor in xActor.Elements("ROW").Elements("COLUMN"))
+        {
+            switch (three)
+            {
+                case 0:
+                    actorList.Add(new Actor());
+                    idx++;
+                    actorList[idx].Actor_id = TrimCDATA(actor.FirstNode.ToString());
+                    three++;
+                    break;
+                case 1:
+                    actorList[idx].Actor_name = TrimCDATA(actor.FirstNode.ToString());
+                    three++;
+                    break;
+                case 2:
+                    actorList[idx].Actor_year = TrimCDATA(actor.FirstNode.ToString());
+                    three = 0;
+                    break;
+            }
+        }
+
+        List<Act> actList = new List<Act>();
+        three = 0;
+        idx = -1;
+        foreach (var i in xAct.Elements("ROW").Elements("COLUMN"))
+        {
+            switch (three)
+            {
+                case 0:
+                    actList.Add(new Act());
+                    idx++;
+                    actList[idx].Act_cast = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 1:
+                    actList[idx].Act_actor_id = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 2:
+                    actList[idx].Act_stage_id = TrimCDATA(i.FirstNode.ToString());
+                    three = 0;
+                    break;
+            }
+        }
+
+        List<Company> companyList = new List<Company>();
+        three = 0;
+        idx = -1;
+        foreach (var i in xCompany.Elements("ROW").Elements("COLUMN"))
+        {
+            switch (three)
+            {
+                case 0:
+                    companyList.Add(new Company());
+                    idx++;
+                    companyList[idx].Company_name = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 1:
+                    companyList[idx].Company_year = TrimCDATA(i.FirstNode.ToString());
+                    three = 0;
+                    break;
+            }
+        }
+
+        List<Contribute> contributeList = new List<Contribute>();
+        three = 0;
+        idx = -1;
+        foreach (var i in xContribute.Elements("ROW").Elements("COLUMN"))
+        {
+            switch (three)
+            {
+                case 0:
+                    contributeList.Add(new Contribute());
+                    idx++;
+                    contributeList[idx].Contribute_stage_id = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 1:
+                    contributeList[idx].Contribute_company_name = TrimCDATA(i.FirstNode.ToString());
+                    three = 0;
+                    break;
+            }
+        }
+
+        List<Creator> creatorList = new List<Creator>();
+        three = 0;
+        idx = -1;
+        foreach (var i in xCreator.Elements("ROW").Elements("COLUMN"))
+        {
+            switch (three)
+            {
+                case 0:
+                    creatorList.Add(new Creator());
+                    idx++;
+                    creatorList[idx].Creator_id = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 1:
+                    creatorList[idx].Creator_name = TrimCDATA(i.FirstNode.ToString());
+                    three = 0;
+                    break;
+            }
+        }
+
+        List<Make> makeList = new List<Make>();
+        three = 0;
+        idx = -1;
+        foreach (var i in xMake.Elements("ROW").Elements("COLUMN"))
+        {
+            switch (three)
+            {
+                case 0:
+                    makeList.Add(new Make());
+                    idx++;
+                    makeList[idx].Make_stage_id = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 1:
+                    makeList[idx].Make_creator_id = TrimCDATA(i.FirstNode.ToString());
+                    three = 0;
+                    break;
+            }
+        }
+
+        List<Play> playList = new List<Play>();
+        three = 0;
+        idx = -1;
+        foreach (var i in xPlay.Elements("ROW").Elements("COLUMN"))
+        {
+            switch (three)
+            {
+                case 0:
+                    playList.Add(new Play());
+                    idx++;
+                    playList[idx].Play_loc = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 1:
+                    playList[idx].Play_stage_id = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 2:
+                    playList[idx].Play_theater_name = TrimCDATA(i.FirstNode.ToString());
+                    three = 0;
+                    break;
+            }
+        }
+
+        List<Stage> stageList = new List<Stage>();
+        three = 0;
+        idx = -1;
+        foreach (var i in xStage.Elements("ROW").Elements("COLUMN"))
+        {
+            switch (three)
+            {
+                case 0:
+                    stageList.Add(new Stage());
+                    idx++;
+                    stageList[idx].Stage_id = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 1:
+                    stageList[idx].Stage_name = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 2:
+                    stageList[idx].Stage_year = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 3:
+                    stageList[idx].Stage_season = TrimCDATA(i.FirstNode.ToString());
+                    three = 0;
+                    break;
+            }
+        }
+
+        List<Theater> theaterList = new List<Theater>();
+        three = 0;
+        idx = -1;
+        foreach (var i in xTheator.Elements("ROW").Elements("COLUMN"))
+        {
+            switch (three)
+            {
+                case 0:
+                    theaterList.Add(new Theater());
+                    idx++;
+                    theaterList[idx].Theater_name = TrimCDATA(i.FirstNode.ToString());
+                    three++;
+                    break;
+                case 1:
+                    theaterList[idx].Theater_location = TrimCDATA(i.FirstNode.ToString());
+                    three = 0;
+                    break;
+            }
+        }
+
+
+        DB.Data().Actors = new List<Actor>(actorList);
+        DB.Data().Acts = new List<Act>(actList);
+        DB.Data().Companies = new List<Company>(companyList);
+        DB.Data().Contributes = new List<Contribute>(contributeList);
+        DB.Data().Creators = new List<Creator>(creatorList);
+        DB.Data().Makes = new List<Make>(makeList);
+        DB.Data().Plays = new List<Play>(playList);
+        DB.Data().Stages = new List<Stage>(stageList);
+        DB.Data().Theaters = new List<Theater>(theaterList);
+
+}
+
+    public string TrimCDATA(string s) {
+        int start = s.IndexOf("D") + 5;
+        int end = s.IndexOf("]") - 1;
+        s = s.Substring(start, end - start + 1);
+        return s;
+    }
+}
---
+++ Assets/Scripts/SearchManager.cs
@@ -0,0 +1,282 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class SearchManager : MonoBehaviour {
+
+    // 0 = 배우, 1 = 공연, 2 = 극장, 3 = 제작자, 4 = 제작사
+    int cate;
+    string search;
+
+    // search key(ex "actor_id"), search keyword(ex "김대종")
+    Dictionary<string, string> searchKey = new Dictionary<string, string>();
+    // result(ex "룽게"), importance(1~5)
+    Dictionary<string, int> searchResult = new Dictionary<string, int>();
+
+    List<Text> searchText = new List<Text>();
+    List<Text> resultText = new List<Text>();
+
+    [SerializeField] Dropdown _category;
+    [SerializeField] InputField _searchBar;
+    [SerializeField] Text _keyword;
+    [SerializeField] GameObject _popup;
+    [SerializeField] Text _searchsearch;
+
+    Transform popup;
+
+    private void Start()
+    {
+        _category = _category.GetComponent<Dropdown>();
+        _searchBar = _searchBar.GetComponent<InputField>();
+        _keyword = _keyword.GetComponent<Text>();
+        popup = _popup.GetComponent<Transform>();
+        _searchsearch = _searchsearch.GetComponent<Text>();
+    }
+
+    public void AddKeyword()
+    {
+        cate = _category.value;
+        search = _searchBar.text;
+        switch (cate)
+        {
+            case 0:
+                var temp0 = from a in DB.Data().Actors
+                        where a.Actor_name == search
+                        select a.Actor_id;
+                foreach (var result in temp0)
+                {
+                    searchKey.Add("actor_id", result);
+                    _keyword.text = _keyword.text + " " + search;
+                }
+                break;
+            case 1:
+                var temp1 = from s in DB.Data().Stages
+                           where s.Stage_name == search
+                           select s.Stage_id;
+                foreach (var result in temp1)
+                {
+                    searchKey.Add("stage_id", result);
+                    _keyword.text = _keyword.text + " " + search;
+                }
+                break;
+            case 2:
+                var temp2 = from t in DB.Data().Theaters
+                           where t.Theater_name == search
+                           select t.Theater_name;
+                foreach (var result in temp2)
+                {
+                    searchKey.Add("theater_name", result);
+                    _keyword.text = _keyword.text + " " + search;
+                }
+                break;
+            case 3:
+                var temp3 = from c in DB.Data().Creators
+                            where c.Creator_name == search
+                            select c.Creator_id;
+                foreach (var result in temp3)
+                {
+                    searchKey.Add("creator_id", result);
+                    _keyword.text = _keyword.text + " " + search;
+                }
+                break;
+            case 4:
+                var temp4 = from c in DB.Data().Companies
+                            where c.Company_name == search
+                            select c.Company_name;
+                foreach (var result in temp4)
+                {
+                    searchKey.Add("company_name", result);
+                    _keyword.text = _keyword.text + " " + search;
+                }
+                break;
+        }
+
+        
+    }
+
+    public void Search() {
+        // 릴레이션이 있다면 그걸 가장 먼저/중요한 결과로 +3
+        // ACT
+        if (searchKey.ContainsKey("actor_id") && searchKey.ContainsKey("stage_id"))
+        {
+            var temp1 = from a in DB.Data().Acts
+                        where a.Act_actor_id == searchKey["actor_id"]
+                        && a.Act_stage_id == searchKey["stage_id"]
+                        select a.Act_cast;
+            foreach (var result in temp1)
+            {
+                searchResult.Add(result, 3);
+            }
+        }
+        // PLAY
+        else if (searchKey.ContainsKey("stage_id") && searchKey.ContainsKey("theater_name"))
+        {
+            var temp2 = from p in DB.Data().Plays
+                        where p.Play_stage_id == searchKey["stage_id"]
+                        && p.Play_theater_name == searchKey["theater_name"]
+                        select p.Play_loc;
+            foreach (var result in temp2)
+            {
+                searchResult.Add(result, 3);
+            }
+        }
+        
+        // 그 외엔 각 키를 포함하는 릴레이션을 순회하며 하나씩 추가. 발견할 때마다 중요도도 +1
+        // actor_id / stage_id / theater_name / creator_name / company_name
+        if (searchKey.ContainsKey("actor_id"))
+        {
+            var tempActor = from a in DB.Data().Actors
+                            where a.Actor_id == searchKey["actor_id"]
+                            select a.Actor_year;
+            foreach (var ee in tempActor)
+                AddResult(ee);
+
+            var tempActor2 = from a in DB.Data().Acts
+                             where a.Act_actor_id == searchKey["actor_id"]
+                             join s in DB.Data().Stages on a.Act_stage_id equals s.Stage_id
+                             select new { cast = a.Act_cast, stage = s.Stage_name };
+            foreach (var ee in tempActor2)
+            {
+                AddResult(ee.cast);
+                AddResult(ee.stage);
+            }
+        }
+        if (searchKey.ContainsKey("stage_id"))
+        {
+            var tempStage = from a in DB.Data().Acts
+                            where a.Act_stage_id == searchKey["stage_id"]
+                            join aa in DB.Data().Actors on a.Act_actor_id equals aa.Actor_id
+                            select new { cast = a.Act_cast, actor = aa.Actor_name };
+            foreach (var ee in tempStage)
+            {
+                AddResult(ee.cast);
+                AddResult(ee.actor);
+            }
+
+            var tempStage2 = from c in DB.Data().Contributes
+                             where c.Contribute_stage_id == searchKey["stage_id"]
+                             join com in DB.Data().Companies on c.Contribute_company_name equals com.Company_name
+                             select new { company = c.Contribute_company_name, year = com.Company_year };
+            foreach (var ee in tempStage2)
+                AddResult(ee.company);
+
+            var tempStage3 = from s in DB.Data().Stages
+                             where s.Stage_id == searchKey["stage_id"]
+                             select s;
+            foreach (var ee in tempStage3)
+            {
+                AddResult(ee.Stage_season);
+                AddResult(ee.Stage_year);
+            }
+
+            var tempStage4 = from m in DB.Data().Makes
+                             where m.Make_stage_id == searchKey["stage_id"]
+                             join cre in DB.Data().Creators on m.Make_creator_id equals cre.Creator_id
+                             select cre.Creator_name;
+            foreach (var ee in tempStage4)
+                AddResult(ee);
+
+            var tempStage5 = from p in DB.Data().Plays
+                             where p.Play_stage_id == searchKey["stage_id"]
+                             join the in DB.Data().Theaters on p.Play_theater_name equals the.Theater_name
+                             select new { loc = p.Play_loc, location = the.Theater_location };
+            foreach (var ee in tempStage5)
+            {
+                AddResult(ee.loc);
+                AddResult(ee.location);
+            }
+        }
+        if (searchKey.ContainsKey("theater_name"))
+        {
+            var tempThe = from p in DB.Data().Plays
+                          where p.Play_theater_name == searchKey["theater_name"]
+                          join s in DB.Data().Stages on p.Play_stage_id equals s.Stage_id
+                          select new { loc = p.Play_loc, stage = s.Stage_name };
+            foreach (var ee in tempThe)
+            {
+                AddResult(ee.loc);
+                AddResult(ee.stage);
+            }
+
+            var tempThe2 = from t in DB.Data().Theaters
+                           where t.Theater_name == searchKey["theater_name"]
+                           select t.Theater_location;
+            foreach (var ee in tempThe2)
+                AddResult(ee);
+
+        }
+        if (searchKey.ContainsKey("creator_id"))
+        {
+            var tempCreator = from m in DB.Data().Makes
+                              where m.Make_creator_id == searchKey["creator_id"]
+                              join sta in DB.Data().Stages on m.Make_stage_id equals sta.Stage_id
+                              select sta.Stage_name;
+            foreach (var ee in tempCreator)
+                AddResult(ee);
+        }
+        if (searchKey.ContainsKey("company_name"))
+        {
+            var tempCompany = from c in DB.Data().Companies
+                              where c.Company_name == searchKey["company_name"]
+                              select c.Company_year;
+            foreach (var ee in tempCompany)
+                AddResult(ee);
+
+            var tempCompany2 = from c in DB.Data().Contributes
+                               where c.Contribute_company_name == searchKey["company_name"]
+                               join sta in DB.Data().Stages on c.Contribute_stage_id equals sta.Stage_id
+                               select sta.Stage_name;
+            foreach (var ee in tempCompany2)
+                AddResult(ee);
+        }
+
+        DrawPopup();
+    }
+
+    private void DrawPopup()
+    {
+        int idx = 0;
+
+        foreach (var result in searchResult)
+        {
+            searchText.Add(_searchsearch);
+            resultText.Add(_searchsearch);
+        }
+
+        foreach (var result in searchResult)
+        {
+            searchText[idx].text = result.Key;
+            searchText[idx].fontSize = 30 + result.Value * result.Value;
+            searchText[idx].color = new Color(0.0f, 0.0f, 0.0f, 0.12f * result.Value);
+
+            resultText[idx] = Instantiate(searchText[idx], new Vector3(Random.Range(100.0f, 900.0f), Random.Range(100.0f, 600.0f), 0), Quaternion.identity) as Text;
+            resultText[idx].transform.SetParent(popup);
+            
+            idx++;
+        }
+    }
+
+    private void AddResult(string s)
+    {
+        if (searchResult.ContainsKey(s))
+            searchResult[s] += 1;
+        else
+            searchResult.Add(s, 1);
+    }
+
+    public void Clear()
+    {
+        searchKey.Clear();
+        searchResult.Clear();
+
+        foreach (var r in resultText)
+            Destroy(r);
+
+        searchText.Clear();
+        resultText.Clear();
+        _keyword.text = "";
+    }
+
+}
